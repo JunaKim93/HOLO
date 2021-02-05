8563e2f53961e4ffb92b93143906fe84cf9f1a7d
aftersnow0105 <seolhu0105@naver.com>
2021-02-05 11:53:10 +0900

    오류수정

--- holo/src/holo/notice/HolonoticeBean.java
+++ holo/src/holo/notice/HolonoticeBean.java
@@ -7,62 +7,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import holo.holouser.HolouserDTO;
 import holo.holouser.service.HolouserService;
+import holo.notice.service.HolonoticeService;
 import spring.model.service.BoardDataBean;
 
 @Controller
 @RequestMapping("/notice/")
 public class HolonoticeBean {
 	@Autowired
-	private HolounoticeService noticeDAO = null;
+	private HolonoticeService noticeDAO = null;
 	
 	@RequestMapping("list.holo")
-	public String list(@RequestParam(defalutValue="1") int pageNum, Model model)throws Exception{
+	public String list(@RequestParam(defaultValue="1") int pageNum, Model model)throws Exception{
 		
 		int pageSize = 10;
 		int currentPage = pageNum;
-		int startRow = (currentPage-1)*
+		int startRow = (currentPage-1)*1;
 
 		return "notice/list";
-
+	}
+		
 		@RequestMapping("writeForm.holo")
 		public String writeForm(HolonoticeDTO dto , Model model)throws Exception {
 			model.addAttribute("dto", dto);
 			return "notice/writeForm";
 		}
-		@RequestMapping("writePro.holo")
-		public String writePro(HolonoticeDTO dto ,HttpServletRequest request)throws Exception {
-			noticeDAO.insertArticle(dto);
-			return "notice/writePro";
-		}
-		
-		@RequestMapping("content.holo")
-		public String content(int num, int pageNum, Model model) throws Exception{
-				HolonoticeDTO article =  noticeDAO.getArticle(num);
-		        model.addAttribute("num", new Integer(num));
-		        model.addAttribute("pageNum", new Integer(pageNum));
-		        model.addAttribute("article", article);
-			return "notice/content";
-		}
-		
-		@RequestMapping("deleteForm.holo")
-		public String deleteForm( ) throws Exception {
-			return "notice/deleteForm";
-		}
-		
-		@RequestMapping("deletePro.holo")
-		public String deletePro(HttpSession session , HolouserDTO member ,Model model) throws Exception {
-		    String id = (String)session.getAttribute("sessionId");
-		    member.setId(id);
-		    int check = noticeDAO.deleteMember(member);
-			if(check==1){
-				session.invalidate();
-			}
-			model.addAttribute("check",check);
-			
-			return "member/deletePro";
-		}
+	/*
+	 * @RequestMapping("writePro.holo") public String writePro(HolonoticeDTO dto
+	 * ,HttpServletRequest request)throws Exception { noticeDAO.insertArticle(dto);
+	 * return "notice/writePro"; }
+	 * 
+	 * @RequestMapping("content.holo") public String content(int num, int pageNum,
+	 * Model model) throws Exception{ HolonoticeDTO article =
+	 * noticeDAO.getArticle(num); model.addAttribute("num", new Integer(num));
+	 * model.addAttribute("pageNum", new Integer(pageNum));
+	 * model.addAttribute("article", article); return "notice/content"; }
+	 * 
+	 * @RequestMapping("deleteForm.holo") public String deleteForm( ) throws
+	 * Exception { return "notice/deleteForm"; }
+	 * 
+	 * @RequestMapping("deletePro.holo") public String deletePro(HttpSession session
+	 * , HolouserDTO member ,Model model) throws Exception { String id =
+	 * (String)session.getAttribute("sessionId"); member.setId(id); int check =
+	 * noticeDAO.deleteMember(member); if(check==1){ session.invalidate(); }
+	 * model.addAttribute("check",check);
+	 * 
+	 * return "member/deletePro"; }
+	 */
 		
 	}
