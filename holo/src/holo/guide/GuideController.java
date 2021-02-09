cc96f53302a4d0ba4011828686dc2678606c66c6
JunaKim93 <kja3369@gmail.com>
2021-02-09 11:15:45 +0900

    지도 api-2

--- holo/src/holo/guide/GuideController.java
+++ holo/src/holo/guide/GuideController.java
@@ -1,14 +1,44 @@
 package holo.guide;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/guide/")
 public class GuideController {
 
 	@RequestMapping("/location_map.holo")
-	public String getMap() {
+	public String getMap(@RequestParam(defaultValue="1",required=true)int location, Model model) {
+		double lat = 0;
+		double lng = 0;
+		switch(location) {
+			case 1: lat =37.566571;
+					lng =126.978584;
+					break;
+			case 2: lat =37.885282;
+					lng =127.729830;
+					break;
+			case 3: lat =37.274895;
+					lng =127.008952;
+					break;
+			case 4: lat =35.871153;
+					lng =128.601681;
+					break;
+			case 5: lat =36.350475;
+					lng =127.384834;
+					break;
+			case 6: lat =35.159975;
+					lng =126.851630;
+					break;
+			case 7: lat =35.179638;
+					lng =129.075087;
+					break;			
+		}
+		model.addAttribute("lat", lat);
+		model.addAttribute("lng", lng);
+		model.addAttribute("location", location);
 		
 		return "guide/location_map";
 	}
